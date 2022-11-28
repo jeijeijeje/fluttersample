c57225292d1720a6de592189e8637f8b06c98f6f
Parabeac-Nest <admin@parabeac.com>
2022-11-28 23:48:30 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/fluttersample_colors.g.dart
@@ -0,0 +1,12 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class FluttersampleColors {
+
+static const Color mainColor = Color(0xff0275e0);
+}
---
+++ lib/theme/fluttersample_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class FluttersampleTextStyles {
+
+static const TextStyle maintext = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
